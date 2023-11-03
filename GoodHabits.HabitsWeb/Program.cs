e4c206a0b03978474cccf0a1a1e2c65d36b58461
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-03 13:05:14 +0530

    Changes

--- GoodHabits.HabitsWeb/Program.cs
+++ GoodHabits.HabitsWeb/Program.cs
@@ -1,4 +1,6 @@
 using GoodHabits.HabitsWeb;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
