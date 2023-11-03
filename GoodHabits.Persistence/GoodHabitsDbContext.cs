d119d6a7920ab0e496427a9e77ce9b1786360011
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-03 12:19:18 +0530

    Changes

--- GoodHabits.Persistence/GoodHabitsDbContext.cs
+++ GoodHabits.Persistence/GoodHabitsDbContext.cs
@@ -1,14 +1,49 @@
 using GoodHabits.Persistence.Entities;
+using GoodHabits.Persistence.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace GoodHabits.Persistence;
 
 public class GoodHabitsDbContext : DbContext
 {
+    private readonly ITenantService _tenantService;
+
+    public string TenantName => _tenantService.GetTenant()?.TenantName ?? string.Empty;
+
     public DbSet<Habit>? Habits => Set<Habit>();
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options) =>
-        options.UseSqlServer("Server=sqlserver;Database=GoodHabitsDatabase;User Id=sa;Password=Sample@123$;Integrated Security=false;TrustServerCertificate=true;");
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) => SeedData.Seed(modelBuilder);
+    public GoodHabitsDbContext(DbContextOptions options, ITenantService service) : base(options) =>
+        _tenantService = service;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var tenantConnectionString = _tenantService.GetConnectionString();
+
+        if (!string.IsNullOrEmpty(tenantConnectionString))
+        {
+            _ = optionsBuilder.UseSqlServer(_tenantService.GetConnectionString());
+        }
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Habit>().HasQueryFilter(a => a.TenantName == TenantName);
+
+        SeedData.Seed(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        ChangeTracker.Entries<IHasTenant>()
+            .Where(entry => entry.State == EntityState.Added || entry.State == EntityState.Modified)
+            .ToList().ForEach(entry => entry.Entity.TenantName = TenantName);
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
 }
