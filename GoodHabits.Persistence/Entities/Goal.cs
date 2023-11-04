a09d5b703d2fe05f502f4c7dcc020ad5c375585c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-04 10:32:42 +0530

    Code Formatting

--- GoodHabits.Persistence/Entities/Goal.cs
+++ GoodHabits.Persistence/Entities/Goal.cs
@@ -8,6 +8,6 @@ public class Goal
     public int Id { get; set; }
 
     public int HabitId { get; set; }
-    
+
     public virtual Habit Habit { get; set; } = default!;
 }
--- GoodHabits.Persistence/Entities/User.cs
+++ GoodHabits.Persistence/Entities/User.cs
@@ -8,8 +8,8 @@ public class User
     public int Id { get; set; }
 
     public string FirstName { get; set; } = default!;
-    
+
     public string LastName { get; set; } = default!;
-    
+
     public string Email { get; set; } = default!;
 }
