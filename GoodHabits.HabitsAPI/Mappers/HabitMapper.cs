6600e9071d0c716949d11be903a80e6cfafc0220
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-05 11:13:00 +0530

    Create HabitMapper.cs

---
+++ GoodHabits.HabitsAPI/Mappers/HabitMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using global::GoodHabits.HabitsAPI.Dtos;
+using global::GoodHabits.Persistence.Entities;
+
+namespace GoodHabits.HabitsAPI.Mappers;
+
+public class HabitMapper : Profile
+{
+    public HabitMapper()
+    {
+        CreateMap<Habit, HabitDto>();
+    }
+}
