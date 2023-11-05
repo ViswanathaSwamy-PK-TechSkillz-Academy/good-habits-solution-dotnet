58490777a44d0d5192ffaa2a8276d6b47360af54
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-05 11:09:16 +0530

    Changes

--- GoodHabits.HabitsAPI/Controllers/HabitsController.cs
+++ GoodHabits.HabitsAPI/Controllers/HabitsController.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using GoodHabits.HabitsAPI.Dtos;
 using GoodHabits.HabitsAPI.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -10,18 +11,24 @@ public class HabitsController : ControllerBase
 {
     private readonly ILogger<HabitsController> _logger;
     private readonly IHabitService _habitService;
+    private readonly IMapper _mapper;
 
-    public HabitsController(ILogger<HabitsController> logger, IHabitService goodHabitsService)
+    public HabitsController(ILogger<HabitsController> logger, IHabitService habitService, IMapper mapper)
     {
-        _logger = logger;
-        _habitService = goodHabitsService;
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+        _habitService = habitService ?? throw new ArgumentNullException(nameof(habitService));
+
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetAsync(int id) => Ok(await _habitService.GetById(id));
+    public async Task<IActionResult> GetAsync(int id) =>
+      Ok(_mapper.Map<HabitDto>(await _habitService.GetById(id)));
 
     [HttpGet]
-    public async Task<IActionResult> GetAsync() => Ok(await _habitService.GetAll());
+    public async Task<IActionResult> GetAsync() =>
+      Ok(_mapper.Map<ICollection<HabitDto>>(await _habitService.GetAll()));
 
     [HttpPost]
     public async Task<IActionResult> CreateAsync(CreateHabitDto request) =>
