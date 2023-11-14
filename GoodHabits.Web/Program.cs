f5ee8401d8505e138885aad955cf921285cfa6e4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-14 15:13:42 +0530

    Code Formatting

--- GoodHabits.Web/Program.cs
+++ GoodHabits.Web/Program.cs
@@ -1,6 +1,6 @@
+using GoodHabits.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using GoodHabits.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
